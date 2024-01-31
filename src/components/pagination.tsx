a13b08dc31ef9838468d03ac8f7392b2e46bbf41
Jake C <jakeychap@gmail.com>
2024-01-31 21:05:22 +0000

    removing pagination to component, adding missing item messages, connecting up logout button to back

---
+++ src/components/pagination.tsx
@@ -0,0 +1,12 @@
+export function Pagination() {
+  return (
+    <div class="flex justify-center mt-4 mb-8">
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full mr-2">
+        1
+      </button>
+      <button class="bg-gray-300 hover:bg-gray-400 text-gray-600 font-semibold py-2 px-4 rounded-full ml-2">
+        2
+      </button>
+    </div>
+  );
+}
