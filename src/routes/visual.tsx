2cae7b0af6f8f2f8d2b4d5637fdeebacb88317aa
Jake C <jakeychap@gmail.com>
2024-02-02 22:02:26 +0000

    visualiser now uses api data

--- src/routes/visual.tsx
+++ src/routes/visual.tsx
@@ -1,23 +1,70 @@
 import { clientOnly } from "@solidjs/start";
 import { createSignal } from "solid-js";
-import { getRoutingData } from "~/rest";
+import { getAllFullBrokerInfo, getRoutingData } from "~/rest";
 
 const Diagram = clientOnly(() => import("~/components/diagram"));
 
 export default function Visualiser() {
   const [loading, setLoading] = createSignal<boolean>(true);
-  const [data, setData] = createSignal<string[]>([]);
+  const [nodes, setNodes] = createSignal<any>([]);
+  const [edges, setEdges] = createSignal<any>([]);
 
-  // first time fetch - in place to avoid css not loading for now, could be issue SSR?
-  getRoutingData("route")
-    .then((values) => {
-      setData(values.queues);
+  getAllFullBrokerInfo()
+    .then((data) => {
+      const ndes = [];
+      const edgs = [];
+
+      for (let i = 0; i < data.length; i++) {
+        let element = data[i];
+
+        ndes.push({
+          id: "node-" + element.key,
+          position: { x: 0, y: 0 },
+          data: {
+            content: (
+              <div>
+                <p class="text-l">Broker: {element.key}</p>
+              </div>
+            ),
+          },
+          inputs: 0,
+          outputs: element.queues.length,
+          color: "#cecece",
+        });
+
+        for (let k = 0; k < element.queues.length; k++) {
+          const queue = element.queues[k];
+          ndes.push({
+            id: "node-" + queue,
+            position: { x: 0, y: 0 },
+            data: {
+              content: (
+                <div>
+                  <p class="text-l">Queue: {queue}</p>
+                </div>
+              ),
+            },
+            inputs: 1,
+            outputs: 0,
+            color: "white",
+          });
+
+          edgs.push({
+            id: "edge_" + element.key + ":" + queue,
+            sourceNode: "node-" + element.key,
+            sourceOutput: k,
+            targetNode: "node-" + queue,
+            targetInput: 0,
+          });
+        }
+      }
+
+      setNodes(ndes);
+      setEdges(edgs);
       setLoading(false);
     })
-    .catch((err) => {
-      console.error(err);
-    });
-    
+    .catch(console.error);
+
   return (
     <div>
       <header class="py-4">
@@ -25,7 +72,7 @@ export default function Visualiser() {
           <h1 class="text-5xl font-semibold">Current Architecture</h1>
         </div>
       </header>
-      <Diagram />
+      {!loading() && <Diagram nodes={nodes()} edges={edges()} />}
     </div>
   );
 }
